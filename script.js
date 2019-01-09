4916a8732feb0cce5578c9aa7b955af4fe30d6de
groonworld <46387880+groonworld@users.noreply.github.com>
2019-01-09 22:20:59 +0100

    Add files via upload
    Added some functionality. The UI needs some improvements, I'm currently working on revealing the sections on button click one step after the other. This makes for a simpler landing page and an improved UX.

--- script.js
+++ script.js
@@ -4,71 +4,168 @@
 /*  Published under MIT License: https://opensource.org/licenses/MIT   */
 /* --------------------------------------------------------------------*/
 
-(function() {  
+
+/********************************************
+ *    CALCULATOR Module                     *
+ *******************************************/
+
+let numberCruncher = (function() {  
+
+    const currency = '&euro';
+    const unit = ' Minuten'
+    return {
+        salaryCalc: function(input) {   
+            if (!isNaN(input.monatsgehalt) && 
+                !isNaN(input.wochenstunden) &&
+                input.monatsgehalt !== '' &&
+                input.wochenstunden !== '') {
+                let workdays = 5;
+                const month = 52/12;
+                
+                return {
+                    hrSalary: input.monatsgehalt / (input.wochenstunden * month),
+                    dSalary: (input.monatsgehalt / (input.wochenstunden * month)) * (input.wochenstunden / workdays),
+                    minSalary: input.monatsgehalt / (input.wochenstunden * month) / 60,
+                    quartSalary: input.monatsgehalt / (input.wochenstunden * month) / 4
+                }
+            } else {
+                console.log('Function salaryCalc() received NaN input.')
+            }
+        },
     
-    let items = {                      // Will soon be expanded to include a range of everyday items people tend 
-        cappuchino: 5,                  // to spend a lot of money on without thinking much about it.
-        custom: -1,                     // "custom" will allow for calculations based on a user-defined price tag.
+        coffeePrice: function(data) {
+            return Math.round(data.items.cappuccino / data.salary.minSalary) + unit;
+        },
+        
+        customPrice: function(data) {
+            return Math.round(data.items.custom / data.salary.minSalary);
+        }
+    }
+})();
+
+
+/********************************************
+ *    UI CONTROLLER Module                  *
+ *******************************************/
+
+let publish = (function() {
+    
+    let currency = {
+        money: '<span>&nbsp;&euro;</span>',
+        time: '<span>&nbsp;min</span>' 
+    };
+    
+    return {          
+        removeElement: function(elementId) {    // Removes an element from the document
+            let element = document.getElementById(elementId);
+            element.parentNode.removeChild(element);
+        },
+
+        getInput: function() {
+            return {
+                monatsgehalt: Number(document.querySelector('.input_gehalt').value.replace(',','.')),
+                wochenstunden: Number(document.querySelector('.input_stunden').value.replace(',','.')),
+            }
+        },
+        
+        getCustom: function() {
+            return Number(document.querySelector('.input_custom').value.replace(',','.'));
+        },
+        
+        update: function(salary, items, prices) {    // Prints all the results to the user interface
+            
+            document.getElementById('value_day').innerHTML = salary.dSalary.toFixed(2) + currency.money;
+            document.getElementById('value_hr').innerHTML = salary.hrSalary.toFixed(2) + currency.money;
+            document.getElementById('value_quart').innerHTML = salary.quartSalary.toFixed(2) + currency.money;
+            document.getElementById('value_min').innerHTML = salary.minSalary.toFixed(2) + currency.money;
+            
+            document.getElementById('output').insertAdjacentHTML('afterbegin', '<div class="row" id="cappuccino_display"><div id="cappuccino_price"><p>Dein geliebter <span>' + items.cappuccino + '&euro;-Mandelmilch-Cappuchino</span> kostet dich <span>' + prices.cappuccino + '</span> deiner Arbeitszeit.</p></div></div>');
+        },
+            
+        revealCustomResult: function(prices) {
+            document.getElementById('value_custom').innerHTML = prices.custom + currency.time;
+        }    
+    }
+})();
+
+
+/********************************************
+ *    GLOBAL APP CONTROLLER Module          *
+ *******************************************/
+
+let controller = (function(calc, ui){
+    //console.log('Setting up Controller module.');
+    const data = {
+        input: {},
+        salary: {},
+        prices: {},
+        items: {                     
+            cappuccino: 5,                          
+            custom: -1,
+        }
     };
     
     function setUpEventListeners() {
-        document.querySelector('.calculate_btn').addEventListener('click', updateUI);
-        document.addEventListener('keypress', function(event) {
-            if (event.keyCode === 13 || event.which === 13){
-                updateUI();
-            }
-        })
-    };
-
-    function getInput() {
-        return {
-            monatsgehalt: Number(document.querySelector('.input_gehalt').value),
-            wochenstunden: Number(document.querySelector('.input_stunden').value),
-        }
-    };
-
-    function calculate() {
-        let input = getInput();
-        let workdays = 5;
-        const month = 52/12;
-        return results = {
-            hrSalary: input.monatsgehalt / (input.wochenstunden * month),
-            dSalary: (input.monatsgehalt / (input.wochenstunden * month)) * (input.wochenstunden / workdays),
-            minSalary: input.monatsgehalt / (input.wochenstunden * month) / 60,
-            quartSalary: input.monatsgehalt / (input.wochenstunden * month) / 4,
-        };
-        logAll(input, results);
-    };
-    
-    function coffeePrice(results, items) {
-        let unit = ' Minuten';
-        return {
-            cappuchino: Math.round((items.cappuchino / results.minSalary)) + unit,
-        }
-    };
-    
-    function updateUI() { // Prints all the results to the user interface
-        let results = calculate();
-        let prices = coffeePrice(results, items);
-        let currency = '<span>&nbsp;€</span>';
-        document.getElementById('value_day').innerHTML = results.dSalary.toFixed(2) + currency;
-        document.getElementById('value_hr').innerHTML = results.hrSalary.toFixed(2) + currency;
-        document.getElementById('value_quart').innerHTML = results.quartSalary.toFixed(2) + currency;
-        document.getElementById('value_min').innerHTML = results.minSalary.toFixed(2) + currency;
         
-        document.getElementById('cappuchino_price').innerHTML = '<p>Dein geliebter ' + items.cappuchino + '&euro;-Mandelmilch-Cappuchino kostet dich <span>' + prices.cappuchino + '</span> deiner Arbeitszeit.</p>';
-    };
-    
-    /* ////////////////// FOR TESTING PURPOSES ONLY ///////////////////////////////////////
-    function logAll(input, results) {
-        console.log('MonatsNettoGehalt: ' + input.monatsgehalt.toFixed(2) + '€');
-        console.log('Wochenstunden: ' + input.wochenstunden.toFixed(2) + '€');
-        console.log(' ');
-        console.log('Tagesverdienst: ' + results.dSalary.toFixed(2) + '€');
-        console.log('Stundenlohn: ' + results.hrSalary.toFixed(2) + '€');
-        console.log('Viertelstundenlohn: ' + results.quartSalary.toFixed(2) + '€');
-        console.log('Minutenlohn: ' + results.minSalary.toFixed(2) + '€');
-    }; //////////////// FOR TESTING PURPOSES ONLY ////////////////////////////////////// */
+        document.getElementById('calculate_btn').addEventListener('click', calculate);
+        document.addEventListener('keypress', function(event) {
+            if (event.keyCode === 13 || event.which === 13){ // KeyCode 13 is the "Enter"-key
+                calculate() 
+            }
+        });
+        document.getElementById('btn_custom_calc').addEventListener('click', custom);
 
-    setUpEventListeners();
-})();
+    }
+    
+    function calculate() {
+        
+        data.input = ui.getInput();
+        data.salary = calc.salaryCalc(data.input);      
+        data.prices.cappuccino = calc.coffeePrice(data);
+        
+        ui.removeElement('cappuccino_display');
+        ui.update(data.salary, data.items, data.prices);
+        console.log(data);
+    }
+    
+    function custom() {
+        data.items.custom = ui.getCustom();
+        data.prices.custom = calc.customPrice(data);
+        ui.revealCustomResult(data.prices);
+    }
+    
+    return {
+        init: function() {
+            setUpEventListeners();
+        },
+        
+        addCustomData: function(number) {
+            data.items.custom = number;
+        },
+    }
+})(numberCruncher, publish);
+
+controller.init();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
